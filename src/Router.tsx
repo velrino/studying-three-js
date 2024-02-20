875dbe7d954b0e675b19a7267217b4a30f25da3e
velrino <denismagalhaesvelrino@gmail.com>
2024-02-20 18:14:07 -0300

    🚧 Installed react-router-dom

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { SolarSystemPage } from './pages/solar-system';
+
+export const RoutesComponent: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' >
+                    <Route path='*' element={<Navigate replace to="/solar-system" />} />
+                    <Route path="/solar-system" element={<SolarSystemPage />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
+};
---
+++ src/pages/solar-system/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function SolarSystemPage()  {
+  return (
+    <div>
+      <h1>Three.js Study Page</h1>
+      <p>Welcome to my 3.js study!</p>
+    </div>
+  );
+};
